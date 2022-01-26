e4acc25c082d5c9565544d54f2e1e4b8cc90b7ff
Ethan <62390197+ethancz@users.noreply.github.com>
2022-01-26 12:07:17 +0100

    Update latitude_sagnac.py

--- latitude_sagnac.py
+++ latitude_sagnac.py
@@ -1,7 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-
-latitude = float(input("A quelle latitude vous trouvez-vous ?"))
+latitude = float(input("A quelle degré de latitude vous trouvez-vous (peu importe l'hémisphère)? : "))
 teta = np.deg2rad(latitude)  # latitude en radian
 R = 6378 * (10 ** 3)  # rayon de la terre en mètre
 t = 86164  # temps en seconde que la terre met pour faire une rotation sur elle-même (23h56mn)
@@ -10,15 +10,19 @@ perimetre = round(perimetre)
 r_latitude = perimetre / (2 * np.pi)    # rayon à la latitude donné
 r_latitude = round(r_latitude)
 v = perimetre / t  # calcul de la vitesse à la latitude donnée en m/s
-w=v/r_latitude     # w la vitesse angulaire à la latitude
+w=v/R     # w la vitesse angulaire à la latitude
 
 
-print("\n\nle périmètre de cette latitude est de", perimetre / 1000, "km et la vitesse a cette latitude est ", round(v), "m/s",
+print("\n\nle périmètre de cette latitude est de", perimetre / 1000, "km et votre vitesse à cette latitude est de", round(v), "m/s",
       "soit", round(v * 3.6), "km/h")
-print("\n\nle rayon à cette latitude est de ", r_latitude / 1000, "km")  # rayon à la latitude donnée en km
-print("\n\nla vitesse angulaire est de w=", w, "rad/s")
+print("\n\nle rayon de cette latitude est de", r_latitude / 1000, "km")  # rayon à la latitude donnée en km
+print("\n\nla vitesse angulaire est de w = ",w, "rad/s")
 
-print("\n\nla valeur de la vitesse angulaire ne dépend donc pas de la latitude, elle est la même \n"
-      "pour tout corps sur terre peu importe sa position géographique")
+print("\n\nla valeur de la vitesse angulaire dépend donc de la latitude")
+
+
+
+
+
 
 
