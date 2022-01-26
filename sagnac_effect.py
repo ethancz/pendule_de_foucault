08fa6765a596f49d38979f8fdb290eae0a617147
Ethan <62390197+ethancz@users.noreply.github.com>
2022-01-26 02:29:40 +0100

    Update sagnac_effect.py

--- sagnac_effect.py
+++ sagnac_effect.py
@@ -18,11 +18,14 @@ def disque_relativite_restreinte(w):  # le paramètre est la vitesse angulaire d
 i =np.arange(0, 20, 1)  #valeur de la vitesse angulaire, axe des abscisses
 graph=disque_relativite_restreinte(i)   #valeur de delta en fonction des valeurs de i, la vitesse angulaire
 point=disque_relativite_restreinte(Omega_Terre) #pour afficher le point d'abscisse Omega_Terre sur le graph
+plt.plot(Omega_Terre,point, marker="x", color="red", markersize=7, label="point ayant pour abscisse la \nvitesse de rotation de la Terre")
+plt.plot(i,graph)
 plt.xlabel('vitesse angulaire (en rad/s)')  #légende de l'axe des abscisses
 plt.ylabel("décalage temporel de l'arrivé des horloges (en s)") #légende de l'axe des ordonnées
-plt.legend() #légende du point
+plt.legend(loc='lower right') #légende du point
 plt.title('Déphasage de Sagnac (en s) en fonction de la vitesse \nangulaire du disque tournant (en rad/s)', fontsize=13,pad=10)
-plt.plot(Omega_Terre,point, marker="o", color="red", markersize=4, label="point ayant pour abscisse la \nvitesse de rotation de la Terre")
-plt.plot(i,graph)
+
 plt.show()
+
+
 
